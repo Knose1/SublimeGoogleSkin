e25fdfaf987ad9563d151bfe2209bb556b16fc0c
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-09-07 01:27:06 +0200

    Create index.js

---
+++ script/index.js
@@ -0,0 +1,9 @@
+//Ag logo
+document.querySelectorAll("img[src='https://www.anime-gate.net/images/anime-manga-download-animegate-logo.svg']").forEach(qsa => {
+  qsa.src = "";
+});
+
+//Ag logo negative
+document.querySelectorAll("img[src='https://www.anime-gate.net/images/anime-manga-download-animegate-logo.svg']").forEach(qsa => {
+  qsa.src = "";
+})
