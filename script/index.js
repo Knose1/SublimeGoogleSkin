64903e140307e509f065c3b544ff828f049d7de1
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-09-07 02:14:51 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,15 +1,15 @@
 //Ag logo
 document.querySelectorAll("img[src='https://www.anime-gate.net/images/anime-manga-download-animegate-logo.svg']").forEach(qsa => {
-  qsa.src = chrome.runtime.getURL("/icons/TrapGateLogo.svg");
+  qsa.src = chrome.runtime.getURL("icons/TrapGateLogo.svg");
 });
 
 //Ag logo negative
 document.querySelectorAll("img[src='https://www.anime-gate.net/images/anime-manga-download-animegate-logo-inverse.svg']").forEach(qsa => {
-  qsa.src = chrome.runtime.getURL("/icons/TrapGateLogoInvert.svg");
+  qsa.src = chrome.runtime.getURL("icons/TrapGateLogoInvert.svg");
 })
 
 document.querySelectorAll("link[rel=icon][href*='/favicon']").forEach(qsa => {
-  qsa.src = chrome.runtime.getURL("/icons/TrapGateImg.png");
+  qsa.src = chrome.runtime.getURL("icons/TrapGateImg.png");
 })
 
 document.head.innerHTML += `
